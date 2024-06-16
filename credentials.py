4cd49bc64e270dfd0880232e1bb635479a2f48ac
iLukSbr <lucasyfm@hotmail.com>
2024-06-16 01:11:46 -0300

    conexão aos bancos de dados

--- credentials.py
+++ credentials.py
@@ -16,12 +16,7 @@ def get_credentials(page):
     cipher_suite = get_key(page)
 
     if not page.client_storage.get("credentials"):
-        credentials = {
-            "host": None,
-            "user": None,
-            "password": None,
-            "database": None
-        }
+        credentials = None
     else:
         cipher_text = page.client_storage.get("credentials").encode()
         decrypted = cipher_suite.decrypt(cipher_text)
@@ -32,5 +27,5 @@ def save_credentials(page, usr_credentials):
     cipher_suite = get_key(page)
     cipher_json = json.dumps(usr_credentials)
     cipher_bytes = cipher_json.encode('utf-8')
-    cipher_text = cipher_suite.encrypt(cipher_bytes)  # Encrypt the credentials
+    cipher_text = cipher_suite.encrypt(cipher_bytes) # Encrypt the credentials
     page.client_storage.set("credentials", cipher_text.decode())
--- main.py
+++ main.py
@@ -5,40 +5,100 @@ import flet as ft
 import credentials
 
 def main(page: ft.Page):
+    #page.client_storage.clear()
+
+    def dialog(text):
+        dlg = ft.AlertDialog(
+            title=ft.Text(
+                text,
+                text_align="CENTER"
+            ),
+
+        )
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
+
+    def draw_db_page():
+        for i in range(7):
+            page.controls.pop()
+        page.update()
+    
+        # tree_button = ft.ElevatedButton(
+        #     text="Árvore de tabelas e views",
+        #     on_click=on_tree_button_click
+        # )
+
+        
+
+    def con_db(usr_credentials):
+        global con
+        connect_functions = {
+            "MySQL": my.connect,
+            "PostgreSQL": pg.connect
+        }
+        connect = connect_functions.get(usr_credentials["database"])
+        try:
+            con = connect(
+                host=usr_credentials["host"],
+                port=int(usr_credentials["port"]),
+                user=usr_credentials["user"],
+                password=usr_credentials["password"]
+            )
+            dialog("Conexão realizada com sucesso.")
+            draw_db_page()
+        except Exception as e:
+            dialog(e)
+
     def on_con_button_click(e):
         if not user_field.value or not password_field.value or not db_field.value:
-            dlg = ft.AlertDialog(
-                title=ft.Text(
-                    "Preencha todos os campos.",
-                    text_align="CENTER"
-                ),
-
-            )
-            page.dialog = dlg
-            dlg.open = True
-            page.update()
+            dialog("Preencha todos os campos.")
         else:
+            if not host_field.value or not port_field.value:
+                host_field.value = "localhost"
+                if db_field.value == "MySQL":
+                    port_field.value = "3306"
+                elif db_field.value == "PostgreSQL":
+                    port_field.value = "5432"
+                page.update()
+            usr_credentials = {
+                "host": host_field.value,
+                "port": port_field.value,
+                "user": user_field.value,
+                "password": password_field.value,
+                "database": db_field.value
+            }
             if save_check.value: # Save credentials
-                credentials.save_credentials(page, {
-                    "host": host_field.value,
-                    "user": user_field.value,
-                    "password": password_field.value,
-                    "database": db_field.value
-                })
-            #con_db(credentials)
-            pass
+                credentials.save_credentials(page, usr_credentials)
+            else: # Clear saved credentials
+                credentials.save_credentials(page, None)
+            con_db(usr_credentials)
     
-    page.title = "Database Interface"
+    page.title = "Interface de Bancos de Dados"
     page.window_height = 720
     page.window_width = 1280
     page.theme_mode = 'LIGHT'
 
     last_credentials = credentials.get_credentials(page)
 
+    if not last_credentials:
+        last_credentials = {
+            "host": "",
+            "port": "",
+            "user": "",
+            "password": "",
+            "database": ""
+        }
+
     # Create text fields for the database credentials
     host_field = ft.TextField(
         label="Host",
         value=last_credentials["host"]
+    )
+
+    port_field = ft.TextField(
+        label="Porta",
+        value=last_credentials["port"]
     )
 
     user_field = ft.TextField(
@@ -55,7 +115,7 @@ def main(page: ft.Page):
 
     save_check = ft.Checkbox(
         label="Salvar credenciais",
-        value=False
+        value=True
     )
 
     db_field = ft.Dropdown(
@@ -75,6 +135,7 @@ def main(page: ft.Page):
 
     page.add(
         host_field,
+        port_field,
         user_field,
         password_field,
         save_check,
