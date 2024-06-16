577f20757068a6c1d477c1abe35dbb0158986d29
iLukSbr <lucasyfm@hotmail.com>
2024-06-16 16:36:51 -0300

    carregamento de tabela

--- d_table.py
+++ d_table.py
@@ -7,9 +7,34 @@ from d_menu import *
 
 # Lista dos bancos de dados disponíveis
 def draw_table_view(page, usr_credentials, con):
+    menubar = menu_bar(page, con)
+
+    # Gerar seletor de tabela
+    def gen_tables_list(db_name, val):
+        try:
+            cursor = con.cursor()
+            if usr_credentials["database"] == "MySQL":
+                cursor.execute(f"USE {db_name};")
+                cursor.execute("SHOW TABLES;")
+            elif usr_credentials["database"] == "PostgreSQL":
+                cursor.execute(f"SET search_path TO {db_name};")
+                cursor.execute("SELECT table_name FROM information_schema.tables;")
+            tables = cursor.fetchall()
+        except Exception as e:
+            display_action(e, page)
+        table_field = ft.Dropdown(
+            label="Tabela",
+            on_change=lambda e: table_selected(e, db_name),
+            value=val
+        )
+        for table in tables:
+            table_field.options.append(ft.dropdown.Option(table[0]))
+        return table_field
+
+    # Um banco de dados foi escolhido
     def db_selected(e):
         db_name = e.control.value
-        table_selector = gen_tables_list(db_name)
+        table_selector = gen_tables_list(db_name, None)
         db_selector = gen_db_list(db_name)
         if db_name:
             table_view = ft.View(
@@ -24,6 +49,7 @@ def draw_table_view(page, usr_credentials, con):
             page.update()
             page.go(table_view.route)
 
+    # Gerar seletor de banco de dados
     def gen_db_list(val):
         try:
             cursor = con.cursor()
@@ -43,30 +69,36 @@ def draw_table_view(page, usr_credentials, con):
             db_field.options.append(ft.dropdown.Option(database[0]))
         return db_field
     
-    def table_selected(e):
-        pass
-
-    def gen_tables_list(db_name):
+    # Uma tabela foi escolhida
+    def table_selected(e, db_name):
+        display_action(f"Carregando a tabela {e.control.value}", page)
         try:
             cursor = con.cursor()
-            if usr_credentials["database"] == "MySQL":
-                cursor.execute(f"USE {db_name};")
-                cursor.execute("SHOW TABLES;")
-            elif usr_credentials["database"] == "PostgreSQL":
-                cursor.execute(f"SET search_path TO {db_name};")
-                cursor.execute("SELECT table_name FROM information_schema.tables;")
-            tables = cursor.fetchall()
+            cursor.execute(f"SELECT * FROM {e.control.value};")
+            table = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
         except Exception as e:
             display_action(e, page)
-        table_field = ft.Dropdown(
-            label="Tabela",
-            on_change=table_selected
+        table_display = ft.DataTable(
+            columns=[ft.DataColumn(ft.Text(name)) for name in column_names],
+            rows=[ft.DataRow(cells=[ft.DataCell(ft.Text(str(value))) for value in row]) for row in table]
         )
-        for table in tables:
-            table_field.options.append(ft.dropdown.Option(table[0]))
-        return table_field
+        table_selector = gen_tables_list(db_name, e.control.value)
+        db_selector = gen_db_list(db_name)
+        if db_name:
+            table_view = ft.View(
+                "/table",[
+                    menubar,
+                    db_selector,
+                    table_selector,
+                    table_display
+                ]
+            )
+            page.views.pop()
+            page.views.append(table_view)
+            page.update()
+            page.go(table_view.route)
 
-    menubar = menu_bar(page, con)
     db_selector = gen_db_list(None)
     db_view = ft.View(
         "/table",[
@@ -77,26 +109,3 @@ def draw_table_view(page, usr_credentials, con):
     page.views.append(db_view)
     page.update()
     page.go(db_view.route)
-
-def table_changed(e):
-    pass
-    # display_action(f"Carregando a tabela {table_field.value}", page)
-    # cursor = con.cursor()
-    # cursor.execute(f"SELECT * FROM {table_field.value};")
-    # table = cursor.fetchall()
-    # table_display = ft.DataTable(
-    #     columns=[
-    #         ft.DataColumn(ft.Text("Coluna")),
-    #         ft.DataColumn(ft.Text("Valor")),
-    #     ],
-    #     rows=[
-    #         ft.DataRow(
-    #             cells=[
-    #                 ft.DataCell(ft.Text(column[0])),
-    #                 ft.DataCell(ft.Text(str(column[1]))),
-    #             ]
-    #         )
-    #         for column in table
-    #     ]
-    # )
-    # page.add(table_display)
